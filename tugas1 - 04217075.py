3225e6bdd866a2f0c48d9aa1d8af9952e693cf5d
clay4freedom <32136950+clay4freedom@users.noreply.github.com>
2017-09-22 09:56:20 +0700

    Tugas Python
    Alfarizi Kurniawan Lesmana
    04217075
    Fasilkom - Sistem Informasi

---
+++ tugas1 - 04217075.py
@@ -0,0 +1,21 @@
+import os
+a="Alfarizi Kurniawan Lesmana"
+b="04217075"
+c=17
+d="Ilmu Komputer"
+e="Sistem Informasi"
+os.system('cls')
+print("|================================================|")
+print("|                                                |")
+print("| Nama              :",a+" |")
+print("| NIM               :",b,"                  |")
+print("| Fakultas          :",d,"             |")
+print("| Prodi             :",e,"          |")
+print("| Umur              :",c,"tahun","                  |")
+print("| Umur 5 tahun lagi :",c+5,"tahun","                  |")
+print("|                                                |")
+print("|================================================|")
+print("")
+print("")
+input("tekan sembarang untuk keluar!")
+os.system('cls')
